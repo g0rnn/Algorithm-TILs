262ae5a135996470fc7ccb79d6761478b4398548
gyunho <124599614+g0rnn@users.noreply.github.com>
2025-06-15 23:42:52 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 132 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -3,29 +3,57 @@ import java.io.*;
 
 public class Main {
 
-    private static int n, m, answer, minKebin = 1_000_000_000;
+    private static final int MAX_INT = 1_000_000_000;
+    private static int n, m, answer, minKebin = MAX_INT;
     private static Set<Integer>[] relationship;
     private static int[] kebin;
+    private static int[][] rel;
 
     public static void main(String[] args) throws IOException {
         input();
-        kebin = new int[n + 1];
+//        kebin = new int[n + 1];
+//
+//        for (int i = 1; i <= n; i++) {
+//            int sum = 0;
+//            for (int j = 1; j <= n; j++) {
+//                sum += trace(i, j, 0);
+//            }
+//            kebin[i] = sum;
+//        }
+//
+//        for (int i = 1; i <= n; i++) {
+//            if (minKebin > kebin[i]) {
+//                minKebin = kebin[i];
+//                answer = i;
+//            }
+//        }
 
-        for (int i = 1; i <= n; i++) {
-            int sum = 0;
-            for (int j = 1; j <= n; j++) {
-                sum += trace(i, j, 0);
+        // 플로이드 와샬
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    rel[i][j] = Math.min(rel[i][j], rel[i][k] + rel[k][j]);
+                }
             }
-            kebin[i] = sum;
         }
 
+        int tmp = MAX_INT;
+        int floydAnswer = 0;
+        
         for (int i = 1; i <= n; i++) {
-            if (minKebin > kebin[i]) {
-                minKebin = kebin[i];
-                answer = i;
+            int total = 0;
+            
+            for (int j = 1; j <= n;j++) {
+                total += rel[i][j];
+            }
+            
+            if (tmp > total) {
+                tmp = total;
+                floydAnswer = i;
             }
         }
-        System.out.println(answer);
+        
+        System.out.println(floydAnswer);
     }
 
     private static int trace(int start, int dest, int dist) {
@@ -56,6 +84,15 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
+        // 플로이드 와샬
+        rel = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                rel[i][j] = MAX_INT;
+                if (i == j) rel[i][j] = 0;
+            }
+        }
+
         relationship = new HashSet[n + 1];
         for (int i = 1; i <= n; i++) {
             relationship[i] = new HashSet<>();
@@ -68,6 +105,10 @@ public class Main {
 
             relationship[a].add(b);
             relationship[b].add(a);
+
+            // 플로이드 와샬
+            rel[a][b] = 1;
+            rel[b][a] = 1;
         }
         br.close();
     }
