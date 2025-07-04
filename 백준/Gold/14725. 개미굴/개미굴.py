46ad31fdb4014575469d5cf7a747c3db616b3787
gyunho <124599614+g0rnn@users.noreply.github.com>
2025-07-04 23:45:04 +0900

    [Gold III] Title: 개미굴, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.py
+++ 백준/Gold/14725. 개미굴/개미굴.py
@@ -1,27 +1,19 @@
 import sys
 input = sys.stdin.readline
-
-class Trie:
-    def __init__(self):
-        self.children = {}
-
-def insert(root, path):
-    node = root
-    for food in path:
-        if food not in node.children:
-            node.children[food] = Trie()
-        node = node.children[food]
-
-def printNode(node, depth):
-    for key in sorted(node.children.keys()):
-        print("--" * depth + key)
-        printNode(node.children[key], depth + 1)
-
 N = int(input())
-root = Trie()
-for _ in range(N):
-    parts = input().split()
-    k = int(parts[0])
-    insert(root, parts[1:])
+trie = {}
 
-printNode(root, 0)
+def dfs(cur, prefix):
+    for k in sorted(cur):
+        print(prefix + k)
+        dfs(cur[k], prefix + "--")
+
+for _ in range(N):
+    n = input().split()
+    cur = trie
+    for i in n[1:]:
+        if i not in cur:
+            cur[i] = {}
+        cur = cur[i]
+
+dfs(trie, "")
