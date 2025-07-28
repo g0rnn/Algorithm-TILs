47a914741c00fca91ac7b7a6abde5af51af34d9a
gyunho <124599614+g0rnn@users.noreply.github.com>
2025-07-28 15:21:59 +0900

    [Gold I] Title: 커피숍2, Time: 2356 ms, Memory: 44900 KB -BaekjoonHub

---
+++ 백준/Gold/1275. 커피숍2/커피숍2.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+
+def init(node, start, end):
+    if start == end:
+        tree[node] = nums[start]
+    else:
+        mid = (start + end) // 2
+        init(node * 2, start, mid)
+        init(node * 2 + 1, mid + 1, end)
+        tree[node] = tree[node * 2] + tree[node * 2 + 1]
+
+def update(node, start, end, idx, val):
+    if start == end:
+        nums[idx] = val
+        tree[node] = val
+    else:
+        mid = (start + end) // 2
+        if start <= idx <= mid:
+            update(node * 2, start, mid, idx, val)
+        else:
+            update(node * 2 + 1, mid + 1, end, idx, val)
+        tree[node] = tree[node * 2] + tree[node * 2 + 1]
+
+def query(node, start, end, l, r):
+    if r < start or end < l:
+        return 0
+    if l <= start and end <= r:
+        return tree[node]
+    mid = (start + end) // 2
+    return query(node * 2, start, mid, l, r) + query(node * 2 + 1, mid + 1, end, l, r)
+
+# 입력
+n, q = map(int, input().split())
+nums = list(map(int, input().split())) ## int는 가변 정수라 long 보다도 커질 수 있음.. 아니 걍 크기제한이 없음
+tree = [0] * (4 * n)
+
+init(1, 0, n-1)
+for _ in range(q):
+    x, y, a, b = map(int, input().split())
+    l = min(x, y) - 1
+    r = max(x, y) - 1
+    
+    print(query(1, 0, n - 1, l, r))
+    update(1, 0, n-1, a-1, b)
+
