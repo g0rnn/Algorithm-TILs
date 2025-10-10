26dc9ce736094b1d969bce45dd15c0b1964c3a9d
gyunho <124599614+g0rnn@users.noreply.github.com>
2025-10-10 18:36:27 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 138.22 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,43 +1,45 @@
+import java.util.*;
+
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = 0;
-        int d = n - 1;
-        int p = n - 1;
+        Deque<Integer> del = new ArrayDeque<>(n);
+        Deque<Integer> pic = new ArrayDeque<>(n);
         
-        while (true) {
-            while (d >= 0 && deliveries[d] == 0) d--;
-            while (p >= 0 && pickups[p] == 0) p--;
-            
-            if (d < 0 && p < 0) break;
-            
-            answer += (Math.max(d, p) * 2L) + 2L;
-            
-            int capD = cap;
-            // 현재 용량을 다 채울 때까지 반복
-            while (d >= 0 && capD > 0) {
-                if (deliveries[d] - capD <= 0) {
-                    capD -= deliveries[d];
-                    deliveries[d] = 0;
-                    d--;
-                } else {
-                    deliveries[d] -= capD;
-                    capD = 0;
-                }
-            }
-            
-            int capP = cap;
-            while (p >= 0 && capP > 0) {
-                if (pickups[p] - capP <= 0) {
-                    capP -= pickups[p];
-                    pickups[p] = 0;
-                    p--;
-                } else {
-                    pickups[p] -= capP;
-                    capP = 0;
-                }
-            }
+        for (int i = 0; i < deliveries.length; i++) {
+            del.push(deliveries[i]);
+            pic.push(pickups[i]);
         }
         
-        return answer;
+        long moved = 0;
+        while (!(del.isEmpty() && pic.isEmpty())) {
+            trimZeros(del);
+            trimZeros(pic);
+            
+            moved += Math.max(del.size(), pic.size());
+            move(del, cap);
+            move(pic, cap);
+        }
+        return moved * 2;
+    }
+    
+    private void trimZeros(Deque<Integer> stack) {
+        while (!stack.isEmpty() && stack.peek() == 0) {
+            stack.pop();
+        }
+    }
+    
+    private void move(Deque<Integer> stack, int cap) {
+        int remain = cap;
+        
+        while (!stack.isEmpty() && remain > 0) {
+            int cnt = stack.pop();
+            if (cnt <= remain) {
+                remain -= cnt;
+            } else {
+                int leftover = cnt - remain;
+                stack.push(leftover);
+                remain = 0;
+            }
+        }
     }
 }
