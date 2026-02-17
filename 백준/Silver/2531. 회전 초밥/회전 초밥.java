4abc13292891099fc4b37249de6f1710682f65d3
gyunho <124599614+g0rnn@users.noreply.github.com>
2026-02-17 17:31:08 +0900

    [Silver I] Title: 회전 초밥, Time: 164 ms, Memory: 18648 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,42 +1,40 @@
-import java.io.*;
 import java.util.*;
-
-// 접시 위엔 여러 종류의 초밥
-// k 개 연속으로 먹으면 할인
-// i번의 쿠폰이 주어짐
-// 먹을 수 있는 초밥의 최대 가짓수
-
-// 접시 수 n, 가짓수 d, 연속 k, 쿠폰번호 c
-
-// n + k  ->> 33000  array
+import java.io.*;
 
 public class Main {
-    static int n, d, k, c;
-    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        arr = new int[n+k];
-        for (int i=0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
-        for (int i=0; i < k; i++) arr[n+i] = arr[i];
+        int n = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        // 시작지점부터 살펴가면서 현재가지 살펴온 종류 + 남은 최대 가짓수 했을 때 안되면 가지치기
-        Set<Integer> set = new HashSet<>();
-        int max = Integer.MIN_VALUE;
-        for (int start = 0; start < n; start++) {
-            for (int i = 0; i < k; i++) set.add(arr[start+i]);
+        int[] dish = new int[n+k+1];
+        int[] cnt = new int[d+1];
+        int distinct = 0;
 
-            int cnt = set.size();
-            if (!set.contains(c)) cnt++;
+        for (int i = 0; i < n; i++) {
+            dish[i] = Integer.parseInt(br.readLine());
 
-            max = Math.max(cnt, max);
-            set.clear();
+            if (i < k) {
+                dish[i+n] = dish[i];
+                if (cnt[dish[i]] == 0) distinct++;
+                cnt[dish[i]]++;
+            }
         }
 
-        System.out.println(max);
+        int result = distinct + ((cnt[c] == 0) ? 1 : 0);
+        for (int i = k; i < n+k; i++) {
+            if (cnt[dish[i]] == 0) distinct++;
+            cnt[dish[i]]++;
+
+            cnt[dish[i-k]]--;
+            if (cnt[dish[i-k]] == 0) distinct--;
+
+            if (cnt[c] == 0) result = Math.max(result, distinct+1);
+            else result = Math.max(result, distinct);
+        }
+        System.out.println(result);
     }
 }
