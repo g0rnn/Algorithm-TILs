886f77f9df9a2a2fff972bc1bf0aeb495667f916
gyunho <124599614+g0rnn@users.noreply.github.com>
2026-04-18 21:33:02 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 136 ms, Memory: 15276 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         input();
         long ans = max - min + 1; // 살펴볼 숫자 개수
-        boolean[] visited = new boolean[(int)(max - min + 2)];
+        boolean[] visited = new boolean[(int)(max - min + 1)];
         long i = 2;
 
         while (i * i <= max) {
@@ -19,11 +19,12 @@ public class Main {
                 start += 1;
             }
 
+            // 제곱 수의 배수에 방문 표시
             for (long j = start; j * pow <= max; j++) {
                 int idx = (int)(j * pow - min);
                 if (!visited[idx]) {
                     visited[idx] = true;
-                    ans--;
+                    ans--; // 방문했으면 정답에서 제외
                 }
             }
             i++;
